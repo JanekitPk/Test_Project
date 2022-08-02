9d9de2780d07e807d92df69e05db2b2d5212a7e3
JanekitPk <6210110051@psu.ac.th>
2022-08-02 11:13:40 +0700

    first commit

---
+++ tests/test_number_utils.py
@@ -0,0 +1,29 @@
+from coe_number.number_utils import is_prime_list
+import unittest
+
+class PrimeListTest(unittest.TestCase):
+    def test_give_2_3_5_is_prime(self):
+        prime_list =[2, 3, 5]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+    def test_give_min_2_3_5_is_prime(self):
+        prime_list = [-2, -3, -5]
+        is_prime = is_prime_list(prime_list)
+        self.assertTrue(is_prime)
+
+    def test_give_0_not_prime(self):
+        prime_list = [0, 1]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_give_4_6_8_not_prime(self):
+        prime_list = [4, 6, 8]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)
+
+    def test_give_min_4_6_8_not_prime(self):
+        prime_list = [-4, -6, -8]
+        is_prime = is_prime_list(prime_list)
+        self.assertFalse(is_prime)        
+
