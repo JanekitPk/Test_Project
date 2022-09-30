f43caac507b1fd55d5cbcf6ca54f057cd0caa867
Janekit <6210110051@psu.ac.th>
2022-09-30 19:47:29 +0700

    change testcipher2

--- tests/testcipher.py
+++ tests/testcipher.py
@@ -48,3 +48,27 @@ class AlternatingCharTest(unittest.TestCase):
         expected_result = 'zYw'
         result = caesarCipher(string,shift_num)
         self.assertEqual(result,expected_result)
+
+class AlternatingCharTest(unittest.TestCase):
+    def test_b_3_give_e(self):
+        string = 'b'
+        shift_num = 3
+        expected_result = 'e'
+        result = caesarCipher(string,shift_num)
+        self.assertEqual(result,expected_result)
+
+class AlternatingCharTest(unittest.TestCase):
+    def test_C_4_give_G(self):
+        string = 'C'
+        shift_num = 4
+        expected_result = 'G'
+        result = caesarCipher(string,shift_num)
+        self.assertEqual(result,expected_result)
+
+class AlternatingCharTest(unittest.TestCase):
+    def test_O_5_give_T(self):
+        string = 'O'
+        shift_num = 5
+        expected_result = 'T'
+        result = caesarCipher(string,shift_num)
+        self.assertEqual(result,expected_result)
