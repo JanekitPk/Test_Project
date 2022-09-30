f6ea52858299893fe8ed277d073354058f8eebbd
Janekit <6210110051@psu.ac.th>
2022-09-30 16:43:43 +0700

    change  testcipher3

--- tests/testcipher.py
+++ tests/testcipher.py
@@ -8,3 +8,35 @@ class AlternatingCharTest(unittest.TestCase):
         expected_result = 'okffng-Qwvb'
         result = caesarCipher(string,shift_num)
         self.assertEqual(result,expected_result)
+
+class AlternatingCharTest(unittest.TestCase):
+    def AxcZ_3_give_DafC(self):
+        string = 'AxcZ+'
+        shift_num = 3
+        expected_result = 'DafC+'
+        result = caesarCipher(string,shift_num)
+        self.assertEqual(result,expected_result)
+
+class AlternatingCharTest(unittest.TestCase):
+    def test_asfsgrja_7_give_hzmznyqh(self):
+        string = 'asfsgrja'
+        shift_num = 7
+        expected_result = 'hzmznyqh'
+        result = caesarCipher(string,shift_num)
+        self.assertEqual(result,expected_result)
+
+class AlternatingCharTest(unittest.TestCase):
+    def test_ndSENGSNnedl657_5_give_siXJSLXSsjiq657(self):
+        string = 'ndSENGSNnedl657'
+        shift_num = 5
+        expected_result = 'siXJSLXSsjiq657'
+        result = caesarCipher(string,shift_num)
+        self.assertEqual(result,expected_result)
+
+class AlternatingCharTest(unittest.TestCase):
+    def test_fdjns14_6_give_ljpty14(self):
+        string = 'fdjns14@'
+        shift_num = 2
+        expected_result = 'ljpty14@'
+        result = caesarCipher(string,shift_num)
+        self.assertEqual(result,expected_result)
