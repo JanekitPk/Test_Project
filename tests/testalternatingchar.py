f6b9e24b0897c2e576d8d6d63a2ca20e971f5fe7
Janekit <6210110051@psu.ac.th>
2022-09-30 15:42:07 +0700

    change testalternatingchar

--- tests/testalternatingchar.py
+++ tests/testalternatingchar.py
@@ -7,3 +7,27 @@ class AlternatingCharTest(unittest.TestCase):
         expected_result = 3
         result = alternatingCharacters(string)
         self.assertEqual(result,expected_result)
+
+    def test_BBBBB_give_4(self):
+        string = 'BBBBB'
+        expected_result = 4
+        result = alternatingCharacters(string)
+        self.assertEqual(result,expected_result)
+
+    def test_ABABABAB_give_0(self):
+        string = 'ABABABAB'
+        expected_result = 0
+        result = alternatingCharacters(string)
+        self.assertEqual(result,expected_result)
+
+    def test_BABABA_give_0(self):
+        string = 'BABABA'
+        expected_result = 0
+        result = alternatingCharacters(string)
+        self.assertEqual(result,expected_result)
+
+    def test_AAABBB_give_4(self):
+        string = 'AAABBB'
+        expected_result = 4
+        result = alternatingCharacters(string)
+        self.assertEqual(result,expected_result)
