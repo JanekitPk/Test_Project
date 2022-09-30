686e5e458d26a53f4f0c057a1b0a2a83c7c67890
Janekit <6210110051@psu.ac.th>
2022-09-30 16:37:22 +0700

    change testcipher2

--- tests/testcipher.py
+++ tests/testcipher.py
@@ -4,7 +4,7 @@ import unittest
 class AlternatingCharTest(unittest.TestCase):
     def test_middleOutz_5_give_okffngQwvb(self):
         string = 'middle-Outz'
-        shift_num = 5
+        shift_num = 2
         expected_result = 'okffng-Qwvb'
         result = caesarCipher(string,shift_num)
         self.assertEqual(result,expected_result)
